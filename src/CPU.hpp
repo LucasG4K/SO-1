57ff540ed0d631cbff3721b1255da47526b0ccd3
Vinicius Alves Pereira <vinicius.apereira04@gmail.com>
2024-10-24 17:45:27 -0300

    code refactor

--- src/CPU.hpp
+++ src/CPU.hpp
@@ -1,31 +1,28 @@
-#ifndef REGISTERBANK
-#define REGISTERBANK
-
-#include "RAM.hpp"
+#ifndef _CPU
+#define _CPU
 
 #include <bits/stdc++.h>
 
-#define dbg(x) cout<<#x<<" = "<<x<<'\n';
+#include "RAM.hpp"
+#include "RegisterBank.hpp"
+
+#define dbg(x) cout << #x << " = " << x << '\n';
 
 using namespace std;
 
-typedef struct Register {
-    int value;
-    bool dirty;
-} Register;
-
 class CPU {
-    private:
-        int PC = 0;
-        Register registers[32]; 
-        string active_instruction;
-        string op;
-    public:
-        void InstructionFetch(vector<string> rom);
-        void InstructionDecode();
-        void Execute();
-        void MemoryAccess(RAM& ram);
-        void WriteBack();
+ private:
+  int PC = 0;
+  RegisterBank register_bank;
+  string active_instruction;
+  string op;
+
+ public:
+  void InstructionFetch(vector<string> rom);
+  void InstructionDecode();
+  void Execute();
+  void MemoryAccess(RAM& ram);
+  void WriteBack();
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,30 @@
 #include "CPU.hpp"
 #include "RAM.hpp"
 
-vector<string> read_ROM(){
-    ifstream codigo("codigo.txt");
-    vector<string> retorno;
-    string temp;
-    while (getline(codigo,temp))
-    {
-        retorno.push_back(temp);
-    }
-    return retorno;
+vector<string> read_ROM() {
+  ifstream codigo("codigo.txt");
+  vector<string> retorno;
+  string temp;
+  while (getline(codigo, temp)) {
+    retorno.push_back(temp);
+  }
+  return retorno;
 }
 
-int main(){
-    int clock=0;
-    vector<string> instrucoes = read_ROM();
-    CPU cpu;
+int main() {
+  int clock = 0;
+  vector<string> instrucoes = read_ROM();
 
-    RAM ram;
-    
-    while (clock>=0)
-    {
-        cpu.InstructionFetch(instrucoes);
-        cpu.InstructionDecode();
-        cpu.Execute();
-        cpu.MemoryAccess(ram);
-    }
+  CPU cpu;
+
+  RAM ram;
+
+  while (clock < instrucoes.size()) {
+    cpu.InstructionFetch(instrucoes);
+    cpu.InstructionDecode();
+    cpu.Execute();
+    cpu.MemoryAccess(ram);
+
+    clock++;
+  }
 }
